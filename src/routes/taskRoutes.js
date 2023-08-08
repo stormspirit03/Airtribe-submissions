d763a9cb1e0529b645826da5221f425e1984974e
stormspirit03 <raviraj03.py@gmail.com>
2023-08-08 16:43:59 +0530

    clean

--- src/routes/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tasksControllerr = require ('../controller/tasks');
-
-router.get('/', tasksControllerr.getAllTasks);
-router.post('/', tasksControllerr.createTask);
-
-module.exports = router;
